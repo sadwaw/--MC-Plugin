422d6f15a3120fe46bc30b43bd7cbbe93b0c175e
春天霹雳 <88182518+sadwaw@users.noreply.github.com>
2023-10-27 09:32:45 +0800

    Add files via upload

---
+++ MCPlugin/签到插件.js
@@ -0,0 +1,11 @@
+// LiteLoader-AIDS automatic generated
+/// <reference path="c:/Users/春天霹雳/Desktop/MCPlugin/dts/helperlib/src/index.d.ts"/> 
+
+ll.registerPlugin(
+    /* name */ "签到插件",
+    /* introduction */ "签到功能",
+    /* version */ [0,0,1],
+    /* otherInformation */ {}
+); 
+
+
